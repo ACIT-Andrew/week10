0f1d374a90b35c177817afddaff7c3aed2d4343f
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-08 15:42:42 -0800

    array methods

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,28 @@ while (a<=1024){
   a=a*2;
 }
 
+// DO/WHILE LOOP
+
 do{
   let input = prompt("what is your name?");
   console.log(Number(input));
 }
-while(!isNaN(Number(input)))
+while(!isNaN(Number(input)));
+
+alert(`Hello ${input}`)
+
+// ARRAY METHODS
+
+let fruits = ['Apple', 'Banana'];
+
+fruits.push('Orange');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.unshift('Mango');
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
